c06d73e968a7733f605bb8c68dc498c15410a570
kkeval <kevalpanchal58@gmail.com>
2022-01-22 14:22:41 +0530

    Initial Commit with Next auth and Chakra ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,21 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import theme from "../theme";
 
-import theme from '../theme'
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+    <SessionProvider session={session}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: false,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
