e0c82d6211c60b853b6372afb44b96952afc0920
Ruchi Ray <ruchiray18@gmail.com>
2022-03-01 01:21:16 +0530

    added review slider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import theme from "../theme";
-
+import './landing-page/landing-page.css'
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
