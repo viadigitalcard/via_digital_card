b7a491c6b410c6cbff1827c699172fc0200eed47
Keval Panchal <34764579+kkeval@users.noreply.github.com>
2022-03-04 00:49:07 +0530

    Changes

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,27 +1,27 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  if (req.nextUrl.pathname === "/userscard") {
-    const session = await getToken({
-      req,
-      secret: process.env.SECRET,
-      //   secureCookie: process.env.NODE_ENV === "development",
-    });
-    // You could also check for any property on the session object,
-    // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("/auth/signin");
-    // If user is authenticated, continue.
-  }
-  if (req.nextUrl.pathname === "/create") {
-    const session = await getToken({
-      req,
-      secret: process.env.SECRET,
-      //   secureCookie: process.env.NODE_ENV === "development",
-    });
-    // You could also check for any property on the session object,
-    // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("/auth/signin");
-    // If user is authenticated, continue.
-  }
-}
+// export async function middleware(req) {
+//   if (req.nextUrl.pathname === "/userscard") {
+//     const session = await getToken({
+//       req,
+//       secret: process.env.SECRET,
+//       //   secureCookie: process.env.NODE_ENV === "development",
+//     });
+//     // You could also check for any property on the session object,
+//     // like role === "admin" or name === "John Doe", etc.
+//     if (!session) return NextResponse.redirect("/auth/signin");
+//     // If user is authenticated, continue.
+//   }
+//   if (req.nextUrl.pathname === "/create") {
+//     const session = await getToken({
+//       req,
+//       secret: process.env.SECRET,
+//       //   secureCookie: process.env.NODE_ENV === "development",
+//     });
+//     // You could also check for any property on the session object,
+//     // like role === "admin" or name === "John Doe", etc.
+//     if (!session) return NextResponse.redirect("/auth/signin");
+//     // If user is authenticated, continue.
+//   }
+// }
