213e1fb419c7a9e849acaa5df519142b9f37b72f
Keval Panchal <34764579+kkeval@users.noreply.github.com>
2022-03-04 11:25:28 +0530

    _middleware updated

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
     const session = await getToken({
       req,
       secret: process.env.SECRET,
-      // secureCookie: process.env.NODE_ENV === "development",
+      secureCookie: process.env.NODE_ENV === "production",
     });
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
@@ -17,7 +17,7 @@ export async function middleware(req) {
     const session = await getToken({
       req,
       secret: process.env.SECRET,
-      // secureCookie: process.env.NODE_ENV === "development",
+      secureCookie: process.env.NODE_ENV === "production",
     });
     // You could also check for any property on the session object,
     // like role === "admin" or name === "John Doe", etc.
