4f680cc830314dec12f86d079bac9e2452a9e7a2
Dhruv Desai <dhruv@bhyve.io>
2022-03-29 15:12:20 +0530

    Added Google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import LandingPage from "../components/Landing/LandingPage";
 import { Navbar } from "../components/Navbar";
+import Script from 'next/script';
 
 const Index = () => (
   <>
@@ -13,6 +14,21 @@ const Index = () => (
         sizes="32x32"
         href="/assets/images/favicon.ico"
       ></link>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-MTB3GT7J8C`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MTB3GT7J8C', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
     </Head>
     <>
       <DarkModeSwitch />
