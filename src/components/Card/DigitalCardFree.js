28f7aa5d6707aa6cf4155d1810e79c62be0f34b8
viadigitalcard <102955339+viadigitalcard@users.noreply.github.com>
2022-05-28 17:31:30 +0530

    Logo update

--- src/components/Card/DigitalCardFree.js
+++ src/components/Card/DigitalCardFree.js
@@ -81,8 +81,8 @@ export const DigitalCardFree = ({ data }) => {
   let url = router.asPath;
 
   const logo = useColorModeValue(
-    "https://file-upload-via-digital.s3.ap-south-1.amazonaws.com/assets/Logo-final.png",
-    "https://file-upload-via-digital.s3.ap-south-1.amazonaws.com/assets/Logo+Dark-final.png"
+    "https://file-upload-via-digital.s3.ap-south-1.amazonaws.com/assets/Logo.png",
+    "https://file-upload-via-digital.s3.ap-south-1.amazonaws.com/assets/LogoDarkFinal.png"
   );
   //Vcard
   function handleSave(e) {
