d52f39b492acbb405043750b2b37037d4f15cbf7
viadigitalcard <102955339+viadigitalcard@users.noreply.github.com>
2024-06-10 19:37:30 +0530

    Update next.config.js
    Spirie Card Redirect

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ async redirects() {
         source: '/card/629af8a9326cb232cfa00b01',
         destination: 'https://fliick.cards/aksjain',
         permanent: true,
+      },
+        {
+        source: '/card/634cf6af604ad3c0bdf69a9d',
+        destination: 'https://fliick.cards/harsh_vasant',
+        permanent: true,
       }
     ]
   },
