b6ae1fa7373bdb51bc348ba1b903f6e5d4cee98a
viadigitalcard <102955339+viadigitalcard@users.noreply.github.com>
2024-06-19 12:12:54 +0530

    Update next.config.js
    Anish Redirect

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ async redirects() {
         source: '/card/634cf6af604ad3c0bdf69a9d',
         destination: 'https://fliick.cards/harsh_vasant',
         permanent: true,
+      },
+        {
+        source: '/card/6555dd19725a92114895bcd2',
+        destination: 'https://fliick.cards/anish_bedmutha',
+        permanent: true,
       }
     ]
   },
