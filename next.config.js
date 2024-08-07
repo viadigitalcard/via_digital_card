2b714ea67ff724ac2ba8bf79b798eba78e459512
viadigitalcard <102955339+viadigitalcard@users.noreply.github.com>
2024-08-07 12:44:11 +0530

    removed aksjain redirect

--- next.config.js
+++ next.config.js
@@ -9,11 +9,7 @@ async redirects() {
         destination: '/auth/signin',
         permanent: true,
       },
-        {
-        source: '/card/629af8a9326cb232cfa00b01',
-        destination: 'https://fliick.cards/aksjain',
-        permanent: true,
-      },
+       
         {
         source: '/card/634cf6af604ad3c0bdf69a9d',
         destination: 'https://fliick.cards/harsh_vasant',
